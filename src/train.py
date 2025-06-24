e9fa5986f921655014761748fe02c9fc1cbce748
bthanh1402 <thanh14022005@gmail.com>
2025-06-24 17:22:19 +0700

    feat: F1 score, minor clean up

--- src/train.py
+++ src/train.py
@@ -3,7 +3,7 @@ import numpy as np
 from ravdess_loader import RAVDESSDataSet, collate_pad
 from models.cnn_lstm   import CRNN
 
-from sklearn.metrics import recall_score, accuracy_score
+from sklearn.metrics import recall_score, accuracy_score, f1_score
 from torch.utils.data import DataLoader
 from pathlib import Path
 
@@ -22,24 +22,32 @@ def train_epoch(model, loader, crit, optim, device):
         losses.append(loss.item())
         preds.append(logits.argmax(1).cpu())
         gts.append(y.cpu())
-    acc = accuracy_score(torch.cat(gts), torch.cat(preds))
-    return np.mean(losses), acc
+    
+    all_preds = torch.cat(preds)
+    all_gts = torch.cat(gts)
+    acc = accuracy_score(all_gts, all_preds)
+    f1 = f1_score(all_gts, all_preds, average='macro')
+    return np.mean(losses), acc, f1
 
 def evaluate(model, loader, crit, device):
-    model.eval
-    losses=[]
-    preds=[]
-    gts=[]
+    model.eval()  
+    losses = []
+    preds = []
+    gts = []
     with torch.no_grad():
-        for x,y,L in loader:
-            x,y,L = x.to(device), y.to(device), L.to(device)
-            logits = model(x,L) 
-            losses.append(crit(logits,y).item())
+        for x, y, L in loader:
+            x, y, L = x.to(device), y.to(device), L.to(device)
+            logits = model(x, L)
+            losses.append(crit(logits, y).item())
             preds.append(logits.argmax(1).cpu())
             gts.append(y.cpu())
-    ua = recall_score(torch.cat(gts), torch.cat(preds), average="macro")
-    acc = accuracy_score(torch.cat(gts), torch.cat(preds))
-    return np.mean(losses), ua, acc
+    
+    all_preds = torch.cat(preds)
+    all_gts = torch.cat(gts)
+    ua = recall_score(all_gts, all_preds, average="macro")
+    acc = accuracy_score(all_gts, all_preds)
+    f1 = f1_score(all_gts, all_preds, average='macro')
+    return np.mean(losses), ua, acc, f1
 
 # ---------------------------
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -65,7 +73,7 @@ if __name__ == "__main__":
 
     best=0
     for epoch in range(1, EPOCHS+1):
-        tr_loss, tr_ac = train_epoch(model, train_dl, crit, optim, DEVICE)
-        val_loss, ua, ac = evaluate(model, val_dl, crit, DEVICE)
+        tr_loss, tr_ac, tr_f1 = train_epoch(model, train_dl, crit, optim, DEVICE)
+        val_loss, ua, ac, val_f1 = evaluate(model, val_dl, crit, DEVICE)
         best = max(best, ac)
-        print(f"Epoch {epoch:02}/{EPOCHS}  train_loss {tr_loss:.3f} | train_accuray {tr_ac:.3f} |  val_accuracy {ac:.3f} | val_UA {ua:.3f} (best ac {best:.3f})")
+        print(f"Epoch {epoch:02}/{EPOCHS}  train_loss {tr_loss:.3f} | train_acc {tr_ac:.3f} | train_f1 {tr_f1:.3f} | val_acc {ac:.3f} | val_UA {ua:.3f} | val_f1 {val_f1:.3f} (best ac {best:.3f})")
